7bfafa187551da7a7b24421dd317ece4c6a90943
Aniket Shirsath <148868961+aniketshirsath@users.noreply.github.com>
2025-02-08 16:25:23 +0530

    Add files via upload

---
+++ Loop Basics/1toN while.cpp
@@ -0,0 +1,22 @@
+//wap to print 1 to n using the while/for a loop.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cout << "Enter Number";
+	cin >> n;
+	
+    int i=1;
+    while(i<=n)
+    {
+    	cout << i << endl;
+    	i++;
+	}
+  
+  
+  
+  return 0;
+}
